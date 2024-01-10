8582296537563cd65c8edd7326f4ff7e739a7e00
maja345eee <121087018+maja345eee@users.noreply.github.com>
2024-01-10 13:55:55 +0100

    buttons

---
+++ Assets/scripts/Maja scripts/gameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("mainScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
