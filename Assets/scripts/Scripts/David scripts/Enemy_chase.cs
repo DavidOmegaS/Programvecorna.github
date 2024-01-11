739a19017013dc9817775384d2a00fa02d733f4a
DavidOmegaS <119925608+DavidOmegaS@users.noreply.github.com>
2024-01-11 16:21:17 +0100

    ssss

--- Assets/scripts/Scripts/David scripts/Enemy_chase.cs
+++ Assets/scripts/Scripts/David scripts/Enemy_chase.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Enemy_chase : MonoBehaviour
 {
     public EnemySight sight;
+    public bool test;
     // Start is called before the first frame update
     void Start()
     {
-        sight = GetComponent<EnemySight>();
+        sight = GetComponentInChildren<EnemySight>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (sight.IsChasing == true)
+        {
+            test = true;
+        }
+        else
+        {
+            test = false;
+        }
     }
 }
